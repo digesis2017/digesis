dcbc1a2905ca60b5c79498f34d9f1ba8a969732f
root <you@example.com>
2017-04-02 11:50:51 -0500

    update

--- application/controllers/Deploy.php
+++ application/controllers/Deploy.php
@@ -1,42 +1,31 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Deploy extends CI_Controller {
-
 	public function __construct() {
 		parent::__construct();
-
 	}
-
-public function index() {
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-$LOCAL_ROOT='/var/www/digesis';
+	public function index() {
+#$LOCAL_ROOT='/var/www/html';
+$LOCAL_ROOT="D:\\wamp/www";
 $LOCAL_REPO_NAME="digesis";
 $LOCAL_REPO="{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
 $REMOTE_REPO="https://github.com/digesis2017/digesis.git";
 $DESIRED_BRANCH="master";
+//var_dump(shell_exec("rmdir {$LOCAL_REPO} /s /q"));
 
 if (file_exists($LOCAL_ROOT)){
-	echo 'paso1';
-	shell_exec("rm -rf {$LOCAL_REPO_NAME}");
-}
-
+	echo 'dddddd';
+$d=shell_exec("rmdir {$LOCAL_REPO} /s /q ");
+	var_dump($d);
+}else
+	echo "holaaa";
+exit;
 if (file_exists($LOCAL_REPO)):
-	echo 'paso2'."cd {$LOCAL_REPO} && git pull";
   echo shell_exec("cd {$LOCAL_REPO} && git pull");
-
 else :  
   echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$DESIRED_BRANCH}");
-echo "cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$DESIRED_BRANCH}";
 endif;
-
 date_default_timezone_set('America/Lima');
 die("done " . date('Y-m-d h:i:s a', time()) . "\n"  ) ;
-
-
 }
 }
--- application/controllers/Encuestas.php
+++ application/controllers/Encuestas.php
@@ -11,7 +11,6 @@ class Encuestas extends CI_Controller {
 	}
 
 	public function index() {
-		echo 'vamos peru';
 		if ( isset($_GET['dni']) && ( !empty($_GET['dni']) ) ) {
 			$this->load->model('mtecnicos');
 			$datat = $this->mtecnicos->tecnicobyDNI($_GET['dni']);
